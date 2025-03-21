29ac23a136d9b0df500ac7d1ec56c7c47d4ad85b
TOMAS CABRAL RIOS <48458658@ort>
2025-03-21 10:35:40 -0300

    desarrollar las funciones

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-function generarNumeroRandom()
+function iniciarJuego()
 {
-    var numeroR = Math.floor(Math.random() * 100) + 1;
+    var numeroR = Math.floor(Math.random() * 100) + 1
+    var intentos = 0
+    
 }
 
 function comprobar()
 {
-    const numero = document.getElementById("NumI").value
-
+    intentos++;
+    const numeroI = document.getElementById("NumI").value
+    if(numeroI < 1 || numeroI > 100)
+    {
+        alert("Por favor ingrese un número del 1 al 100");
+    }
+    
+    else
+    {
+        if(numeroR == numeroI)
+        {
+            alert("Ganaste!");
+        }
+        else if(numeroI > numeroR)
+        {
+            if(numeroI - numeroR > 40)
+            {
+                alert("El numero que ingresaste es mucho mayor");
+            }
+            else if (numeroI - numeroR > 20)
+            {
+                alert("El numero que ingresaste es mayor");
+            }
+            else
+            {
+                alert("El numero que ingresaste es un poco mayor");
+            }
+        }
+        else
+        {
+            if(numeroR - numeroI > 40)
+            {
+                alert("El numero que ingresaste es mucho menor");
+            }
+            else if (numeroR - numeroI > 20)
+            {
+                alert("El numero que ingresaste es menor");
+            }
+            else
+            {
+                alert("El numero que ingresaste es un poco mnenor");
+            }
+        }
+        document.getElementById("contador").innerHTML = ("Intentos: " + intentos)
+    }
 }
