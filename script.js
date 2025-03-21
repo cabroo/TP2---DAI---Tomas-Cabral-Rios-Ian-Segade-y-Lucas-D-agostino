463a430bdd0c0f4e990ff1554c4389e1abc47a1c
TOMAS CABRAL RIOS <48458658@ort>
2025-03-21 08:37:43 -0300

    creacion de funciones generarNumeroRandom() y comprobar()

---
+++ script.js
@@ -0,0 +1,10 @@
+function generarNumeroRandom()
+{
+    var numeroR = Math.floor(Math.random() * 100) + 1;
+}
+
+function comprobar()
+{
+    const numero = document.getElementById("NumI").value
+
+}
